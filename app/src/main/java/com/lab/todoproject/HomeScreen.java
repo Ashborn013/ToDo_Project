4d38b3754dc121d222f55110e50a93a37a92d5ac
Ashborn013 <csarjun11@gmail.com>
2024-11-09 09:57:12 +0530

    HomeScreen And AddTask

--- app/src/main/java/com/lab/todoproject/HomeScreen.java
+++ app/src/main/java/com/lab/todoproject/HomeScreen.java
@@ -1,12 +1,16 @@
 package com.lab.todoproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class HomeScreen extends AppCompatActivity {
 
@@ -15,6 +19,14 @@ public class HomeScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home_screen);
+
+        FloatingActionButton fltb = findViewById(R.id.floatingActionButton2);
+        fltb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), AddTask.class));
+            }
+        });
 
     }
 }
